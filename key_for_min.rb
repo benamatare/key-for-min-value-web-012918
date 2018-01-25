59c8f40b48b8753c2e259e96531b9f83da99b632
Ben Matare <ben.a.matare@gmail.com>
2018-01-25 02:00:48 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,12 +6,12 @@ def key_for_min_value(name_hash)
     holder_key = nil
     holder_value = nil
   name_hash.select do |key, value|
-    if holder_value == 0
+    if holder_value == nil
       holder_value = value
       holder_key = key
     elsif holder_value > value
-    holder_value = value
-    holder_key = key
+      holder_value = value
+      holder_key = key
 end
 end
 
